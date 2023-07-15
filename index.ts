280608eb153553c3b2d2703a8aba87fc4d825809
olegpl1993 <olegpl1993@gmail.com>
2023-07-16 02:52:45 +0300

    feat: add cors

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 // Конфигурация прокси
 const proxyOptions = {
-  target: 'http://example.com', // Замените на целевой сервер, на который будете перенаправлять запросы
+  target: 'https://msdrop.com.ua', // Замените на целевой сервер, на который будете перенаправлять запросы
   changeOrigin: true, // Изменить заголовок "Origin" на целевой сервер
   // Дополнительные опции прокси можно указать здесь
 };
+
+// Использование пакета cors
+app.use(cors());
 
 // Применение прокси-сервера к определенному пути
 app.use('/api', createProxyMiddleware(proxyOptions));
